5894d3f295e001a6cfbc0f927f639d938b694711
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-21 16:06:41 -0400

    employee test build

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,2 +1,35 @@
+const { hasUncaughtExceptionCaptureCallback } = require('process');
+const { TestScheduler } = require('rxjs/testing');
 const employee = require('../lib/employee');
 
+test('creates a new employee object', () => {
+    const employee = new employee('Amy', 1, 'amy@testmail.com')
+
+    expect(employee.name).toBe('Amy')
+    expect(employee.id).toEqual(1)
+    expect(employee.email).toBe('amy@testmail.com')
+})
+
+test('gets the employees name', () => {
+    const employee = new employee('Amy')
+    
+    expect(employee.getName()).toBe('Amy')
+})
+
+test('gets employee id', () => {
+    const employee = new employee('Amy')
+
+    expect(employee.getId()).toEqual(1)
+})
+
+test('gets employee email address', () => {
+    const employee = new employee('Amy')
+
+    expect(employee.getEmail()).toBe('amy@testmail.com')
+})
+
+test('gets employee role', () => {
+    const employee = new employee('Amy')
+
+    expect(employee.getRole()).toBe('employee')
+})
