b233594a1e53243ad1439e47af1d7efccb3be168
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-21 17:00:32 -0400

    intern tests

--- tests/intern.test.js
+++ tests/intern.test.js
@@ -1,2 +1,51 @@
-const intern = require('../lib/intern');
+const { TestScheduler } = require('rxjs/testing');
+const Intern = require('../lib/intern');
 
+const natalie = {
+    name: 'Natalie',
+    id: 6,
+    email: 'natalie@testemail.com', 
+    school: 'Liberty University'
+}
+
+const { name, id, email, school } = natalie
+
+test('intern created', () => {
+    const intern = new Intern(name, id, email, school)
+
+    expect(intern.name).toBe(name)
+    expect(intern.id).toEqual(id)
+    expect(intern.email).toBe(email)
+    expect(intern.role).toBe('Intern')
+    expect(intern.school).toBe(school)
+})
+
+test('gets intern name', () => {
+    const intern = new Intern(name, id, email, school)
+
+    expect(intern.getName()).toBe(name)
+})
+
+test('gets intern id', () => {
+    const intern = new Intern(name, id, email, school)
+
+    expect(intern.getId()).toEqual(id)
+})
+
+test('gets intern email address', () => {
+    const intern = new Intern(name, id, email, school)
+
+    expect(intern.getEmail()).toBe(email)
+})
+
+test('gets role', () => {
+    const intern = new Intern(name, id, email, school)
+
+    expect(intern.getRole()).toBe('Intern')
+})
+
+test('gets school name', () => {
+    const intern = new Intern(name, id, email, school)
+
+    expect(intern.getSchool()).toBe(school)
+})
