61a1f581e3ecdcedf958a043c5b4f2f574b6801c
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-21 16:17:28 -0400

    employee tests

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -2,34 +2,43 @@ const { hasUncaughtExceptionCaptureCallback } = require('process');
 const { TestScheduler } = require('rxjs/testing');
 const employee = require('../lib/employee');
 
+const Amy = {
+    name: 'Amy'
+    id: 1, 
+    email: 'amy@testemail.com'
+}
+
+const { name, id, email } = Amy
+
 test('creates a new employee object', () => {
     const employee = new employee('Amy', 1, 'amy@testmail.com')
 
-    expect(employee.name).toBe('Amy')
-    expect(employee.id).toEqual(1)
-    expect(employee.email).toBe('amy@testmail.com')
+    expect(employee.name).toBe(name)
+    expect(employee.id).toEqual(id)
+    expect(employee.email).toBe(email)
+    expect(employee.role).toBe('employee')
 })
 
 test('gets the employees name', () => {
-    const employee = new employee('Amy')
+    const employee = new employee(name)
     
-    expect(employee.getName()).toBe('Amy')
+    expect(employee.getName()).toBe(Amy)
 })
 
 test('gets employee id', () => {
-    const employee = new employee('Amy')
+    const employee = new employee(Amy)
 
-    expect(employee.getId()).toEqual(1)
+    expect(employee.getId()).toEqual(id)
 })
 
 test('gets employee email address', () => {
-    const employee = new employee('Amy')
+    const employee = new employee(Amy)
 
-    expect(employee.getEmail()).toBe('amy@testmail.com')
+    expect(employee.getEmail()).toBe(email)
 })
 
 test('gets employee role', () => {
-    const employee = new employee('Amy')
+    const employee = new employee(Amy)
 
     expect(employee.getRole()).toBe('employee')
 })
