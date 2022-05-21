e3f924e9d03ce9506b3a8fa6ae6c6d73f02b7e62
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-21 16:33:16 -0400

    engineer test created

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,1 +1,58 @@
+const { hasUncaughtExceptionCaptureCallback } = require('process');
 const engineer = require('../lib/engineer');
+const engineer = require('../lib/engineer');
+const engineer = require('../lib/engineer');
+const engineer = require('../lib/engineer');
+const engineer = require('../lib/engineer');
+const engineer = require('../lib/engineer');
+
+const tom = {
+    name: 'Tom', 
+    id: 3, 
+    email: 'tom@testemail.com'
+    role: 'Engineer'
+    github: 'tomexample'
+}
+
+const {name, id, email, github} = tom
+
+test('creates engineer object', () => {
+    const engineer = new engineer(name, id, email, github)
+
+    expect(engineer.name).toBe(name)
+    expect(engineer.id).toEqual(id)
+    expect(engineer.email).toBe(email)
+    expect(engineer.role).toBe('Engineer')
+    expect(engineer.github).toBe(github)
+})
+
+test('gets engineer name', () => {
+    const engineer = new engineer(tom)
+
+    expect(engineer.getName()).toBe(name)
+})
+
+test('gets engineer id', () => {
+    const engineer = new engineer(tom)
+
+    expect(engineer.getId()).toEqual(id)
+})
+
+test('gets engineer email', () => {
+    const engineer = new engineer(tom)
+
+    expect(engineer.getEmail()).toBe(email)
+})
+
+test('gets role', () => {
+    const engineer = new engineer(tom)
+
+    expect(engineer.getRole()).toBe('Engineer')
+})
+
+test('gets github id', () => {
+    const engineer = new engineer(tom)
+
+    expect(engineer.getGithub()).toBe(github)
+})
+
