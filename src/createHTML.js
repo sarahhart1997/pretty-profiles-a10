05110392ae5553373639daeb53231dd448213570
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-22 15:37:56 -0400

    rough code for HTML creation

--- src/createHTML.js
+++ src/createHTML.js
@@ -1,13 +1,27 @@
-function renderManagers() {
-
-}
-
-function renderEngineers() {
-
-}
-
-function renderInterns() {
-
+function showEmployee = (data) => {
+    ${employeesArr
+        .filter(({ role }) => role ==='Employee')
+        .map(({ name, id, email, role })) => {
+        return `
+    <!-- Card 1 Employee --> 
+        <div class="person-card" style="width: 18rem; min-height: 12rem">
+            <div class="card-header text-light bg-primary">
+                <h4 class="card-title">${name}</h4>
+                <p class="person-role">
+                    Employee
+                </p>
+            </div> 
+            <div class="card-body bg-light">
+                <div class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+                </div>
+            </div>
+        </div>
+        `;
+    })
+    .join('')}
+    
 }
 
 module.exports = createHtml;
