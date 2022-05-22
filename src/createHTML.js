31f01c46c372c841cbd3728ee513d7fb6f28c98a
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-22 15:44:27 -0400

    rough code

--- src/createHTML.js
+++ src/createHTML.js
@@ -1,9 +1,8 @@
 function showEmployee = (data) => {
     ${employeesArr
         .filter(({ role }) => role ==='Employee')
-        .map(({ name, id, email, role })) => {
+        .map(({ name, id, email, role }) => {
         return `
-    <!-- Card 1 Employee --> 
         <div class="person-card" style="width: 18rem; min-height: 12rem">
             <div class="card-header text-light bg-primary">
                 <h4 class="card-title">${name}</h4>
@@ -21,7 +20,76 @@ function showEmployee = (data) => {
         `;
     })
     .join('')}
-    
+
+    ${employeesArr
+        .filter(({ role }) => role ==='Engineer')
+        .map(({ name, id, email, role, github }) => {
+        return `
+        <div class="person-card" style="width: 18rem; min-height: 12rem">
+            <div class="card-header text-light bg-primary">
+                <h4 class="card-title">${name}</h4>
+                <p class="person-role">
+                    Employee
+                </p>
+            </div> 
+            <div class="card-body bg-light">
+                <div class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+                    <li class="list-group-item">Github: ${github} </li>
+                </div>
+            </div>
+        </div>
+        `;
+    })
+    .join('')}
+
+    ${employeesArr
+        .filter(({ role }) => role ==='Intern')
+        .map(({ name, id, email, role, school }) => {
+        return `
+        <div class="person-card" style="width: 18rem; min-height: 12rem">
+            <div class="card-header text-light bg-primary">
+                <h4 class="card-title">${name}</h4>
+                <p class="person-role">
+                    Employee
+                </p>
+            </div> 
+            <div class="card-body bg-light">
+                <div class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+                    <li class="list-group-item">Github: ${school} </li>
+                </div>
+            </div>
+        </div>
+        `;
+    })
+    .join('')}
+
+    ${employeesArr
+        .filter(({ role }) => role ==='Manager')
+        .map(({ name, id, email, role, github }) => {
+        return `
+        <div class="person-card" style="width: 18rem; min-height: 12rem">
+            <div class="card-header text-light bg-primary">
+                <h4 class="card-title">${name}</h4>
+                <p class="person-role">
+                    Employee
+                </p>
+            </div> 
+            <div class="card-body bg-light">
+                <div class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+                    <li class="list-group-item">Github: ${officeNumber} </li>
+                </div>
+            </div>
+        </div>
+        `;
+    })
+    .join('')}
+
 }
 
 module.exports = createHtml;
